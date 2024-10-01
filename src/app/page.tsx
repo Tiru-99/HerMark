cc28231639a6d05a651d63e85e96441f28807359
tiru-99 <aayushtirmanwar1234@gmail.com>
2024-10-01 21:27:51 +0530

    unwanted components removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 
 import Frontcomp from '@/components/Frontcomp';
 import Form from '@/components/Form';
-import Books from '@/components/Books';
-import Authorcomp from '@/components/Authorcomp';
+
 import Masonry from '@/components/Masonry';
 import PricingCard from '@/components/PricingCard';
 import AboutUs from '@/components/Aboutus';
@@ -16,8 +15,7 @@ export default function Home() {
   return (
     <div>
       <Frontcomp></Frontcomp>
-      <Books></Books>
-      <Authorcomp></Authorcomp>
+      
       <div className='mt-24'>
         <Form></Form>
       </div>
