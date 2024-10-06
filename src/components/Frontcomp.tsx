c55e6e4170b390c50175cd6eb54bc8a627b01c7f
tiru-99 <aayushtirmanwar1234@gmail.com>
2024-10-06 14:24:52 +0530

    logo added on frontcomp

--- src/components/Frontcomp.tsx
+++ src/components/Frontcomp.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Frontcomp() {
   const [isHovered, setIsHovered] = useState(false);
-
+  
   return (
     <div className="w-full h-[100vh] relative">
       <Image
@@ -15,19 +15,32 @@ export default function Frontcomp() {
         objectFit="cover"
         className={`${isHovered ? 'grayscale-0' : 'grayscale'} transition-all duration-300 ease-in-out`}
       />
-
+      
+      {/* Added logo */}
+      <div className="absolute top-5 left-4 md:left-20 z-10">
+        <Image
+          src="/logo-white3.png"
+          alt="Logo"
+          width={260}
+          height={90}
+          className="object-contain"
+        />
+      </div>
+      
       <div className="absolute bottom-[20%] left-4 md:left-20 w-full">
         <div className="text-4xl md:text-7xl font-thin text-white text-left md:text-left font-serif">
           Making <span className={`${isHovered ? 'text-pink-400' : 'text-white'} transition-all duration-300 ease-in-out`}>Her</span> Mark in <br />Literature
         </div>
-
-        <Link to='getstarted' smooth ={true} duration={500}><button
-          className="text-white border p-3 mt-6 md:mt-12 border-white text-lg md:text-2xl hover:bg-white hover:text-black transition-all duration-300 ease-in-out"
-          onMouseEnter={() => setIsHovered(true)} // Set hovered state to true on mouse enter
-          onMouseLeave={() => setIsHovered(false)} // Set hovered state to false on mouse leave
-        >
-          Get started with publishing
-        </button></Link>
+        
+        <Link to='getstarted' smooth={true} duration={500}>
+          <button
+            className="text-white border p-3 mt-6 md:mt-12 border-white text-lg md:text-2xl hover:bg-white hover:text-black transition-all duration-300 ease-in-out"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            Get started with publishing
+          </button>
+        </Link>
       </div>
     </div>
   );
