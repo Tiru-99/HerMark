5b9e50f81134fe3bf3fc55f4861602f2cfabf49c
tiru-99 <aayushtirmanwar1234@gmail.com>
2024-10-06 21:54:17 +0530

    social media icons added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
-import { Send, MessageCircle } from 'lucide-react';
+import { Send, MessageCircle, Instagram, Linkedin } from 'lucide-react';
 
 export default function Footer() {
   return (
     <footer className="bg-white text-black p-8 md:mt-24 mt-12 mx-4 font-sans border-2 border-pink-500">
       <div className="container mx-auto">
         <div className="flex flex-col md:flex-row justify-between items-start mb-8">
-          <div className="mb-6 md:mb-0 md:mr-12"> {/* Moved mr-12 here */}
+          <div className="mb-6 md:mb-0 md:mr-12">
             <div className="flex items-center mb-4">
               <img src='/logoblack3.png' className='scale-75' alt="Logo" />
             </div>
@@ -17,10 +17,9 @@ export default function Footer() {
                 <li>Privacy</li>
                 <li>FAQ</li>
                 <li>About Us</li>
-                <li>Partners</li>    
+                <li>Partners</li>
               </ul>
             </div>
-            
           </div>
         </div>
         <div className="flex flex-col md:flex-row justify-between items-center pt-8 border-t border-gray-200">
@@ -28,11 +27,17 @@ export default function Footer() {
             <a className="p-2 bg-black text-white rounded-full">
               <Send size={20} />
             </a>
-            <a className="p-2 bg-black text-white rounded-full"  href="https://wa.me/971555378328?text=I%20am%20interested%20in%20the%20Premium%20Plan%20For%20HerMark.">
+            <a className="p-2 bg-black text-white rounded-full" href="https://wa.me/971555378328?text=I%20am%20interested%20in%20the%20Premium%20Plan%20For%20HerMark.">
               <MessageCircle size={20} />
             </a>
+            <a className="p-2 bg-black text-white rounded-full" href="https://www.instagram.com/hermarkpress?igsh=ejR5NG9wYTlqeHdu" target="_blank" rel="noopener noreferrer">
+              <Instagram size={20} />
+            </a>
+            <a className="p-2 bg-black text-white rounded-full" href="https://www.linkedin.com/company/hermark-press/" target="_blank" rel="noopener noreferrer">
+              <Linkedin size={20} />
+            </a>
           </div>
-          <p className="text-sm  font-bold text-left">
+          <p className="text-sm font-bold text-left">
             Phone: +971 55 537 8328<br />Address: Office 906, Park Regis, Business Bay, Dubai
           </p>
         </div>
